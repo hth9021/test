47eae0e9e22d987aba32f8741c9dc891801690fd
wjd5480 <wjd5480@gmail.com>
2018-04-10 16:45:42 +0900

    R_code_update

--- inchone-Airport-Congestion-Removal-Guide-master/inchone-Airport-Congestion-Removal-Guide-master/incheonAirport_Congestion/18년 04월 10일 인천공항 예상승객수.R
+++
@@ -1,6 +0,0 @@
-install.packages('readxl')
-library(readxl)
-K<-read_excel(file.choose())
-View(K)
-
-plot(K$`sum`,type='h', ylim=c(0,10000))
--- inchone-Airport-Congestion-Removal-Guide-master/inchone-Airport-Congestion-Removal-Guide-master/incheonAirport_Congestion/USA_1987_AirInfo.R
+++
@@ -1,4 +0,0 @@
-library("csv")
-air<-read.csv(file.choose())
-air
-plot(air$ArrDelay,type="h",ylim=c(0,500))
